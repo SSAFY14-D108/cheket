ac4ad24a9c562683b15ac0b2e3eca1096c1c799b
황효주 <hjoo830@naver.com>
2026-03-03 13:41:27 +0900

    [BE] init: Spring Boot 초기 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/cheket/CheketApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cheket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/cheket/CheketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cheket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
