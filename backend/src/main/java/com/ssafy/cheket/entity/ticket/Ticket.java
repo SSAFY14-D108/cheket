eba43f265d09aa953ff5c710041753b918f24797
윤진원 <dnj1510@naver.com>
2026-03-12 14:13:18 +0900

    [BE] feat: 볼 예정인 티켓 목록 조회 API 구현

--- backend/src/main/java/com/ssafy/cheket/entity/ticket/Ticket.java
+++ backend/src/main/java/com/ssafy/cheket/entity/ticket/Ticket.java
@@ -15,7 +15,11 @@ import java.time.LocalDateTime;
 public class Ticket {
 
     public enum ResaleStatus {
-        AVAILABLE, SOLD, LISTED, USED, EXPIRED
+        AVAILABLE, // 구매 완료, 보유 중
+        SOLD, // 구매 완료, 보유 중(단순 보유)
+        LISTED, // 리세일 마켓에 등록 됨
+        USED, // QR 체크인 완료
+        EXPIRED // 공연 종료 후 미사용
     }
 
     @Id
--- backend/src/main/java/com/ssafy/cheket/service/ticket/TicketService.java
+++ backend/src/main/java/com/ssafy/cheket/service/ticket/TicketService.java
@@ -1,8 +1,15 @@
 package com.ssafy.cheket.service.ticket;
 
+import com.ssafy.cheket.dto.ticket.response.GetUpcomingTicketResponse;
+
+import java.util.List;
+
 public interface TicketService {
 
     // 티켓 환불
     void refundTicket(Long ticketId);
+
+    // 볼 예정인 티켓 조회
+    List<GetUpcomingTicketResponse> getUpcomingTickets(Long id);
 
 }
