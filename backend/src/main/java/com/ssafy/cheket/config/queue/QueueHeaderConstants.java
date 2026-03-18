b51a96eb50df3ffe369b0617868332fe6ce89e33
윤진원 <dnj1510@naver.com>
2026-03-18 13:17:11 +0900

    [BE] feat: 대기열 API 구현 전 공통 로직 작성

---
+++ backend/src/main/java/com/ssafy/cheket/config/queue/QueueHeaderConstants.java
@@ -0,0 +1,11 @@
+package com.ssafy.cheket.config.queue;
+
+public class QueueHeaderConstants {
+
+    private QueueHeaderConstants() {
+    }
+
+    public static final String QUEUE_TOKEN_HEADER = "Queue-Token";
+    public static final String SEAT_ACCESS_TOKEN_HEADER = "Seat-Access-Token";
+
+}
